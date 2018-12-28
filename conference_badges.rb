d294f6098b591ff521a8ef58f95475a69a783733
Ismail Jalloh <ijalloh21@gmail.com>
2018-12-28 08:25:43 +0000

    Automatically backed up by Learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -29,7 +29,9 @@ def assign_rooms (speakers)
 end
 
 def printer (attendees)
-  puts batch_badge_creator (attendees)
-  puts assign_rooms (attendees)
+  attendees.each do |speaker|
+    puts batch_badge_creator (attendees)
+    puts assign_rooms (attendees)
+  end
 end
   
